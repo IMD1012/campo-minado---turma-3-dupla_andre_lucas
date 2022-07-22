bddd19efdebfc07da75541942d6eb3a7e043bc5e
LucasCAbrantes <45038685+LucasCAbrantes@users.noreply.github.com>
2022-07-22 10:45:58 -0300

    Delete constantes.d

--- constantes.d
+++
@@ -1,3 +0,0 @@
-constantes.o: campo-minado---turma-3-dupla_andre_lucas-main/constantes.c \
-  campo-minado---turma-3-dupla_andre_lucas-main/constantes.h \
-  campo-minado---turma-3-dupla_andre_lucas-main/estruturas.h
