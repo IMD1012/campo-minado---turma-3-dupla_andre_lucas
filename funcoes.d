4a55fc034b23a0fcc50edf9da933dcbffba52824
LucasCAbrantes <45038685+LucasCAbrantes@users.noreply.github.com>
2022-07-22 10:46:18 -0300

    Delete funcoes.d

--- funcoes.d
+++
@@ -1,4 +0,0 @@
-funcoes.o: campo-minado---turma-3-dupla_andre_lucas-main/funcoes.c \
-  campo-minado---turma-3-dupla_andre_lucas-main/estruturas.h \
-  campo-minado---turma-3-dupla_andre_lucas-main/constantes.h \
-  campo-minado---turma-3-dupla_andre_lucas-main/funcoes.h
